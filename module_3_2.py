831b294f8a82d5cae00c5f55200d94be047d0464
SMOG01 <PavlovPavel94@yandex.ru>
2024-07-25 19:03:52 +0300

    Способы вызова функции

--- module_3_2.py
+++ module_3_2.py
@@ -29,7 +29,14 @@ def send_email(message: str, recipient: str, *, sender=default_mail):
         return
 
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('Это сообщение для проверки связи',
+           'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!',
+           'urban.fan@mail.ru',
+           sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание',
+           'urban.student@mail.ru',
+           sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре',
+           'urban.teacher@mail.ru',
+           sender='urban.teacher@mail.ru')
