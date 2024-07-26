c5805fc4d9660285994b6f7899696fec38457532
SMOG01 <PavlovPavel94@yandex.ru>
2024-07-26 19:59:59 +0300

    А как делить?

---
+++ module_4_1.py
@@ -0,0 +1,9 @@
+import fake_math as f
+import true_math as t
+
+print(f.divide(124, 0))
+print(f.divide(24, 2))
+print(f.divide(56, 12))
+print(t.divide(124, 0))
+print(t.divide(3, 14))
+print(t.divide(53, 3))
---
+++ true_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        return '∞'
+    else:
+        return first / second
