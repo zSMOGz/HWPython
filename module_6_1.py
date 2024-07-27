4fe216fbe2ee3a75eb3bc5a12cb06f390aea335d
SMOG01 <PavlovPavel94@yandex.ru>
2024-07-27 20:59:56 +0300

    Свой YouTube

---
+++ module_6_1.py
@@ -0,0 +1,56 @@
+class Plant:
+    edible = False
+
+    def __init__(self,
+                 name):
+        self.name = name
+
+
+class Flower(Plant):
+    edible = False
+
+
+class Fruit(Plant):
+    edible = True
+
+
+class Animal:
+    alive = True
+    fed = False
+
+    def __init__(self,
+                 name):
+        self.name = name
+
+    def eat(self,
+            food: Plant):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
