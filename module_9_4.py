f6e7745c340febe7e58cdea2a9c041624ea37c59
SMOG01 <PavlovPavel94@yandex.ru>
2024-07-30 09:14:44 +0300

    Функциональное разнообразие

---
+++ module_9_4.py
@@ -0,0 +1,37 @@
+from random import choice
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map((lambda x, y: x == y), first, second)))
+print('')
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'w', encoding='utf-8') as file:
+            for line in data_set:
+                file.write(str(line) + '\n')
+                print(str(line))
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+print('')
+
+
+class MysticBall:
+    def __init__(self,
+                 *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
