4a32a7314d3a920a6b1a4747c58020cca7de047f
SMOG01 <PavlovPavel94@yandex.ru>
2024-07-31 06:34:03 +0300

    Try и Except, исправление

--- module_8_1.py
+++ module_8_1.py
@@ -2,7 +2,7 @@ def add_everything_up(a, b):
     try:
         return a + b
     except TypeError:
-        return f'{a}{b}'
+        return str(a)+str(b)
 
 
 print(add_everything_up(123.456, 'строка'))
