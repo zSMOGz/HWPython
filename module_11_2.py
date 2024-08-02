ce393f2e609ce987babc8598a6f6a37465d8e37f
SMOG01 <PavlovPavel94@yandex.ru>
2024-08-02 23:00:17 +0300

    Интроспекция

---
+++ module_11_2.py
@@ -0,0 +1,28 @@
+import inspect
+import pprint
+
+
+class Unknown:
+    def __init__(self):
+        self.__x = 0
+
+    def getInfo(self):
+        return 'Не скажу'
+
+def introspection_info(obj):
+    type_object = type(obj)
+    attr_object = inspect.getmembers(obj)
+    methods_object = inspect.getmembers(obj)
+    module = inspect.getmodule(obj)
+    doc = inspect.getdoc(obj)
+    info = {'type': type_object,
+            'attributes': attr_object,
+            'methods': methods_object,
+            'module': module,
+            'documentation': doc}
+    return info
+
+
+unknown = Unknown()
+number_info = introspection_info(unknown)
+pprint.pprint(number_info)
