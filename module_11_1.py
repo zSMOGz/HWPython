364da53042b64acf38c92b4a125caa8a05a65f51
SMOG01 <PavlovPavel94@yandex.ru>
2024-08-02 15:53:53 +0300

    Обзор сторонних библиотек Python

--- module_11_1.py
+++ module_11_1.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-np.random.seed(19680801)  # seed the random number generator.
+np.random.seed(5254)
 data = {'a': np.arange(50),
         'c': np.random.randint(0, 50, 50),
         'd': np.random.randn(50)}
 data['b'] = data['a'] + 10 * np.random.randn(50)
 data['d'] = np.abs(data['d']) * 100
 
-fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
+fig, ax = plt.subplots(figsize=(5, 5), layout='constrained')
 ax.scatter('a', 'b', c='c', s='d', data=data)
 ax.set_xlabel('entry a')
 ax.set_ylabel('entry b')
