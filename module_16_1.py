18bd64f88c7d9f132b69e73d0a9b8fb172b43454
SMOG01 <PavlovPavel94@yandex.ru>
2024-08-12 13:00:48 +0300

    Основы Fast Api и маршрутизация

---
+++ module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def main() -> dict:
+    return {"message": "Главная страница"}
+
+
+@app.get("/user/admin")
+async def main() -> dict:
+    return {"message": "Вы вошли как администратор"}
+
+
+@app.get("/user/{user_id}")
+async def main(user_id: int) -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+@app.get("/user")
+async def id_paginator(user_name: str, age: int) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {user_name}, Возраст: {age}"}
